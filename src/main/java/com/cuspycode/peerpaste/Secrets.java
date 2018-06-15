bcd50c2428a597907424ce5875349eb925caf5e0
Bjorn Danielsson <bjorn@cuspycode.se>
2018-06-15 19:46:35 +0200

    Use JSON for peer settings

--- src/main/java/com/cuspycode/peerpaste/Secrets.java
+++ src/main/java/com/cuspycode/peerpaste/Secrets.java
@@ -5,17 +5,63 @@ import java.util.LinkedHashMap;
 import java.security.SecureRandom;
 import java.util.Base64;
 
+import org.json.JSONObject;
+import org.json.JSONException;
+
 public class Secrets {
     private static SecureRandom rng = new SecureRandom();
 
-    private static Map<String,String> ephemeralMap = new LinkedHashMap<String,String>();
+    private static Map<String,Entry> ephemeralMap = new LinkedHashMap<String,Entry>();
 
-    public static String getSecret(String peerName) {
+    public static class Entry {
+	public String secret;
+	public long created;
+	public int version;
+
+	public Entry(String secret, long created, int version ) {
+	    this.secret = secret;
+	    this.created = created;
+	    this.version = version;
+	}
+
+	public static Entry fromJSON(String json) {
+	    Entry entry = null;
+	    try {
+		JSONObject obj = new JSONObject(json);
+		entry = new Entry(obj.getString("secret"), obj.getLong("created"), obj.getInt("version"));
+	    } catch (JSONException e) {
+		entry = null;
+	    }
+	    return entry;
+	}
+
+	public String toJSON() {
+	    String result = null;
+	    JSONObject obj = new JSONObject();
+	    try {
+		obj.put("secret", secret);
+		obj.put("created", created);
+		obj.put("version", version);
+		result = obj.toString();
+	    } catch (JSONException e) {
+		result = null;
+	    }
+	    return result;
+	}
+    }
+
+    public static Entry getEntry(String peerName) {
 	return ephemeralMap.get(peerName);
     }
 
+    public static String getSecret(String peerName) {
+	Entry entry = getEntry(peerName);
+	return (entry != null? entry.secret : null);
+    }
+
     public static void putSecret(String peerName, String secret) {
-	ephemeralMap.put(peerName, secret);
+	Entry entry = new Entry(secret, System.currentTimeMillis(), 1);
+	ephemeralMap.put(peerName, entry);
     }
 
     public static void removeSecret(String peerName) {
@@ -28,7 +74,7 @@ public class Secrets {
 	return Base64.getEncoder().encodeToString(bytes);
     }
 
-    public static Map<String,String> getAllSecrets() {
+    public static Map<String,Entry> getAllSecrets() {
 	return ephemeralMap;
     }
 }
